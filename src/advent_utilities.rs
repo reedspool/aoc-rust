b7c067bd51c4bbfedb7b71930cb0bd4212d0fa61
Reed Spool <reed@heysparkbox.com>
2021-11-30 21:50:18 -0700

    Utility for comparing arrays of i32s

--- src/advent_utilities.rs
+++ src/advent_utilities.rs
@@ -1,6 +1,16 @@
 use std::fs;
 
+#[allow(dead_code)]
 pub fn read_input(filename: &str) -> String {
     fs::read_to_string(format!("./inputs/{}.txt", filename))
         .expect("Something went wrong reading the file")
+}
+
+#[allow(dead_code)]
+pub fn assert_array_eq(arr1 : &[i32], arr2: &[i32]) {
+   assert_eq!(arr1.len(), arr2.len());
+
+    for (index, item) in arr1.iter().enumerate() {
+        assert_eq!(item, &arr2[index]);
+    }
 }
