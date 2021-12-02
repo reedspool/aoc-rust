a576ae879ab8db8e6a0defc5d1fada9f84425ad1
Reed Spool <reed@heysparkbox.com>
2021-12-01 20:54:06 -0700

    First attempt at 2021 day 1, w a little refactoring

--- src/aoc_2021/challenge_1.rs
+++ src/aoc_2021/challenge_1.rs
@@ -1,18 +1,54 @@
 #[allow(dead_code)]
 pub fn part_1(input: String) -> i32 {
-    0
+    let mut numbers = input
+        .trim()
+        .split("\n")
+        .map(|s| -> i32 { s.trim().parse().unwrap() });
+
+    // Peel off the first value first, can't compare the first to nothing
+    let mut last = numbers.next().unwrap();
+    let mut count = 0;
+    for num in numbers {
+        if num > last {
+            count += 1
+        }
+        last = num
+    }
+
+    count
 }
 
 #[allow(dead_code)]
 pub fn part_2(input: String) -> i32 {
-    0
-}
+    let mut numbers = input
+        .trim()
+        .split("\n")
+        .map(|s| s.trim().parse::<i32>().unwrap());
 
+    let mut last2 = numbers.next().unwrap();
+    let mut last1 = numbers.next().unwrap();
+    let mut last = numbers.next().unwrap();
+    let mut last_total = last + last1 + last2;
+    let mut current_total;
+    let mut count = 0;
+    for num in numbers {
+        current_total = num + last + last1;
+        if current_total > last_total {
+            count += 1
+        }
+        last2 = last1;
+        last1 = last;
+        last = num;
+        last_total = last + last1 + last2;
+    }
+
+    count
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::advent_utilities::read_input;
     use super::*;
+    use crate::advent_utilities::read_input;
 
     #[test]
     fn test_part_1() {
@@ -21,7 +57,7 @@ mod tests {
         let result = part_1(input.clone());
 
         println!("My answer is {}", result);
-        assert_eq!(result, 0);
+        assert_eq!(result, 1162);
     }
 
     #[test]
@@ -31,6 +67,6 @@ mod tests {
         let result = part_2(input.clone());
 
         println!("My answer is {}", result);
-        assert_eq!(result, 0);
+        assert_eq!(result, 1190);
     }
 }
